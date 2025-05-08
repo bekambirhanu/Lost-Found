23c213c3dd76d949c8e086ef06f8ede7b7336ba9
Mahred1 <mredwan.kemal@gmail.com>
2025-05-08 15:20:00 +0300

    - Implement tabbed navigation with state management - Add FAB for post creation with proper positioning - Integrate 3 core pages (Home, Search, Account) - Follow Material Design guidelines for icons and layout

---
+++ lib/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'account.dart';
+import 'add_post.dart';
+import 'post_view_page.dart';
+import 'search.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
